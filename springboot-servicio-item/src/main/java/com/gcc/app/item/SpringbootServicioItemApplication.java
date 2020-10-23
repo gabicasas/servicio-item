f3172210fc0632c6779660dd2dc29c5b869a9f1e
Gabriel Casas <gabicasas@gmail.com>
2020-10-23 11:47:51 +0200

    escalado dinamico

--- springboot-servicio-item/src/main/java/com/gcc/app/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/gcc/app/item/SpringbootServicioItemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
-@RibbonClient(name="servicio-productos")
+//@RibbonClient(name="servicio-productos") // Ya no hace falta con Eureka
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
