09e80f6d449705ec6beb0498785c97185564b228
Gabriel Casas <gabicasas@gmail.com>
2020-10-23 13:15:32 +0200

    tolerancia_fallos_hystrix

--- springboot-servicio-item/src/main/java/com/gcc/app/item/models/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/gcc/app/item/models/controllers/ItemController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gcc.app.item.models.Item;
+import com.gcc.app.item.models.Producto;
 import com.gcc.app.item.models.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
@@ -23,10 +25,17 @@ public class ItemController {
 		List<Item> result=itemService.findAll();
 		return result;
 	}
-	
+	@HystrixCommand(fallbackMethod = "detalleFallback")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
+	}
+	
+	public Item detalleFallback( Long id, Integer cantidad) {
+		Producto p= new Producto();
+		p.setNombre("error");
+		p.setPrecio(-1.0);
+		return new Item(p,-1);
 	}
 
 }
